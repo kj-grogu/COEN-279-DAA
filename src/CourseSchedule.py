08386c659d78bc66efcfc9cb03768ee18df1ca08
Bharti Prakash <104809666+kj-grogu@users.noreply.github.com>
2024-05-09 07:13:13 -0700

    mod leetcode 207, 210
    modified code for problems course schedule 1 and 2. using topological sort

--- src/CourseSchedule.py
+++ src/CourseSchedule.py
@@ -1,7 +1,3 @@
-from ast import List
-from typing import List
-
-class CourseSchedule:
 #  207. Course Schedule
 #  https://leetcode.com/problems/course-schedule/
 
@@ -23,33 +19,93 @@ class CourseSchedule:
 # Explanation: There are a total of 2 courses to take. 
 # To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
 
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #step1: map each course to its prereqs
-        preMap = {i:[] for i in range(numCourses)} # a map of length of numCourses is creates and initialzed with empty array
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre) #as prerequisites is of format [0,1] where 1st element is course and 2nd element is prereq, here we append prereq course in the list of prereq courses for that particular course in preMap for every particular entry in prerequisites.
+from ast import List
+import collections
+from typing import List
+
+# class CourseSchedule:
+#     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+#         #step1: map each course to its prereqs
+#         preMap = {i:[] for i in range(numCourses)} # a map of length of numCourses is creates and initialzed with empty array
+#         for crs, pre in prerequisites:
+#             preMap[crs].append(pre) #as prerequisites is of format [0,1] where 1st element is course and 2nd element is prereq, here we append prereq course in the list of prereq courses for that particular course in preMap for every particular entry in prerequisites.
            
-        #step2: visit each course along the current course DFS path
-        #visitSet = all the courses along the current DFS path
-        visitSet = set()
-        def dfs(crs): #recursive function to visit all nodes depth first in a path
-            if crs in visitSet:
-                return False #in current dfs path if crs is present in visitSet then a cycle has been detected and return false
-            if preMap[crs] == []: 
-                return True #return true if the current crs dfs path has no prerequisite courses
+#         #step2: visit each course along the current course DFS path
+#         #visitSet = all the courses along the current DFS path
+#         visitSet = set()
+#         def dfs(crs): #recursive function to visit all nodes depth first in a path
+#             if crs in visitSet:
+#                 return False #in current dfs path if crs is present in visitSet then a cycle has been detected and return false
+#             if preMap[crs] == []: 
+#                 return True #return true if the current crs dfs path has no prerequisite courses
             
-            visitSet.add(crs)
-            for pre in preMap[crs]:
-                if not dfs(pre): return False #return false if dfs call to one of the prereq courses in the crrent dfs path return false
+#             visitSet.add(crs)
+#             for pre in preMap[crs]:
+#                 if not dfs(pre): return False #return false if dfs call to one of the prereq courses in the crrent dfs path return false
             
-            visitSet.remove(crs) #remove the current course from visitSet as we will be moving to next course which might not have a path from current course but current course might have a path from it, if we leave it in the set this will trigger false alarm for a cycle encounter
-            preMap[crs] = [] #This is save execution time, if we have already established that a course can be completed then the whole dfs process needs not to be done for it again.
+#             visitSet.remove(crs) #remove the current course from visitSet as we will be moving to next course which might not have a path from current course but current course might have a path from it, if we leave it in the set this will trigger false alarm for a cycle encounter
+#             preMap[crs] = [] #This is save execution time, if we have already established that a course can be completed then the whole dfs process needs not to be done for it again.
+#             return True
+
+#         for crs in range(numCourses):
+#             if not dfs(crs): return False
+#         return True
+
+# OR:
+
+class CourseSchedule:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Early return if no prerequisites are given, all courses can be finished independently
+        if not prerequisites:
             return True
 
-        for crs in range(numCourses):
-            if not dfs(crs): return False
+        # Create a graph from the prerequisites list
+        self.graph = collections.defaultdict(list)
+        for course, pre_req in prerequisites:
+            self.graph[course].append(pre_req)
+
+        # Sets to keep track of visitation states: white (unvisited), grey (visiting), black (visited)
+        white = set(self.graph.keys())  # initially, all nodes are unvisited, unvisited set of nodes
+        grey = set() # currently visiting set of nodes
+        black = set() # visited set of nodes
+
+        # Process all unvisited nodes
+        while white:
+            course = white.pop()
+
+            if not self.dfs(course, grey, black):
+                return False
+        
         return True
     
+    def dfs(self, course, grey, black):
+        """
+        Depth-first search to detect cycles in the graph.
+        """
+        # Mark the current node as visiting (grey)
+        grey.add(course)
+
+        # Explore the prerequisites of the current course
+        for pre_req in self.graph[course]:
+            if pre_req in black:  # already fully processed node
+                continue
+            if pre_req in grey:  # found a cycle
+                return False
+            if not self.dfs(pre_req, grey, black):  # recurse into the prerequisite
+                return False
+        
+        # Mark the current node as visited (black), remove from visiting (grey)
+        grey.remove(course)
+        black.add(course)
+        return True
+
+# Complexity:
+# T: O(V + E) - where V is the number of vertices (courses) and E is the number of edges (prerequisites).
+# The algorithm must visit each node and explore each edge in the worst case.
+# S: O(V + E) - space for the graph and the recursion stack in worst case scenario.
+
+    
+        
 # Testing
 instance = CourseSchedule()
 numCourses = 2
--- src/CourseSchedule2.py
+++ src/CourseSchedule2.py
@@ -1,6 +1,3 @@
-from ast import List
-from typing import List
-class CourseSchedule2:
 # 210. Course Schedule II
 # https://leetcode.com/problems/course-schedule-ii/
 
@@ -33,39 +30,104 @@ class CourseSchedule2:
 # ai != bi
 # All the pairs [ai, bi] are distinct.
 
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # build the adjacency list of prereqs
-        prereq = {i:[] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            prereq[crs].append(pre)
+from ast import List
+from typing import List
+# class CourseSchedule2:
+#     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+#         # build the adjacency list of prereqs
+#         prereq = {i:[] for i in range(numCourses)}
+#         for crs, pre in prerequisites:
+#             prereq[crs].append(pre)
         
-        # a course has 3 stages:
-        # visited -> course has been added to the output and removed from cycle
-        # visiting -> course not added to the output but added to the cycle
-        # unvisited -> crs not added to the output or cycle
+#         # a course has 3 stages:
+#         # visited -> course has been added to the output and removed from cycle
+#         # visiting -> course not added to the output but added to the cycle
+#         # unvisited -> crs not added to the output or cycle
 
-        output = []
-        visit, cycle = set(), set()
-        def dfs(crs):
-            if crs in cycle:
-                return False
-            if crs in visit:
-                return True
+#         output = []
+#         visit, cycle = set(), set()
+#         def dfs(crs):
+#             if crs in cycle:
+#                 return False
+#             if crs in visit:
+#                 return True
 
-            cycle.add(crs)
-            for pre in prereq[crs]:
-                if dfs(pre) == False: 
-                    return False
-            cycle.remove(crs)
-            visit.add(crs)
-            output.append(crs)
-            return True
+#             cycle.add(crs)
+#             for pre in prereq[crs]:
+#                 if dfs(pre) == False: 
+#                     return False
+#             cycle.remove(crs)
+#             visit.add(crs)
+#             output.append(crs)
+#             return True
 
-        for crs in range(numCourses):
-            if dfs(crs) == False: 
+#         for crs in range(numCourses):
+#             if dfs(crs) == False: 
+#                 return []
+#         return output
+
+# OR:
+
+class CourseSchedule2:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # If there are no prerequisites, return all courses in the simplest order
+        if not prerequisites:
+            return [num for num in range(numCourses)]
+
+        # Create a graph where each course points to its prerequisites
+        self.graph = {num: [] for num in range(numCourses)}
+        for course, pre_req in prerequisites:
+            self.graph[course].append(pre_req)
+
+        # Sets to manage DFS states: unvisited (white), visiting (grey), and visited (black)
+        white = set(self.graph.keys())  # initially all nodes are unvisited
+        grey = set()
+        black = set()
+
+        self.order = []  # This will store the courses in the correct order
+
+        # Process each course that has not been visited yet
+        while white:
+            course = white.pop()
+
+            # Skip if already processed
+            if course in black:
+                continue
+
+            # Perform DFS, if cycle is found, return empty list
+            if not self.dfs(course, grey, black):
                 return []
-        return output
+
+        # If all courses are processed without finding a cycle, return the order
+        return self.order
     
+    def dfs(self, course, grey, black):
+        # Mark the current node as visiting (grey)
+        grey.add(course)
+
+        # Check all prerequisites of the current course
+        for pre_req in self.graph[course]:
+            if pre_req in black:
+                continue  # Skip if already fully processed
+            if pre_req in grey:
+                return False  # Cycle detected
+            if not self.dfs(pre_req, grey, black):
+                return False
+        
+        # All prerequisites processed, add course to order
+        self.order.append(course)
+        # Mark course as visited (move from grey to black)
+        grey.remove(course)
+        black.add(course)
+
+        return True
+
+# Complexity:
+# Time (T): O(V + E) - where V is the number of vertices (courses) and E is the number of edges (prerequisites).
+# The algorithm must visit each node and explore each edge in the worst case.
+# Space (S): O(V + E) - space for the graph, the recursion stack in worst case scenario, and administrative data.
+
+
 # Testing
 instance = CourseSchedule2()
 numCourses = 2
