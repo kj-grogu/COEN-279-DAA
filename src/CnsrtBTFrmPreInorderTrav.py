4aeb033c979db106fb61442d896a2e771162733f
Bharti Prakash <104809666+kj-grogu@users.noreply.github.com>
2024-05-16 22:10:51 -0700

    leetcode 105
    CnsrtBTFrmPreInorderTrav solved

---
+++ src/CnsrtBTFrmPreInorderTrav.py
@@ -0,0 +1,62 @@
+# 105. Construct Binary Tree from Preorder and Inorder Traversal
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+# Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, 
+# construct and return the binary tree.
+
+
+# Example 1:
+# Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+# Output: [3,9,20,null,null,15,7]
+
+# Example 2:
+# Input: preorder = [-1], inorder = [-1]
+# Output: [-1]
+ 
+
+# Constraints:
+# 1 <= preorder.length <= 3000
+# inorder.length == preorder.length
+# -3000 <= preorder[i], inorder[i] <= 3000
+# preorder and inorder consist of unique values.
+# Each value of inorder also appears in preorder.
+# preorder is guaranteed to be the preorder traversal of the tree.
+# inorder is guaranteed to be the inorder traversal of the tree.
+
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder and not inorder:
+            return None
+        
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+
+        return root
+
+# Complexity:
+# T: O(N^2) In the worst case, the algorithm may visit each node in both the preorder and inorder traversal, 
+#           leading to a quadratic time complexity. 
+#           This occurs when the tree is skewed, 
+#           and each recursive call involves iterating through a significant portion of the inorder list to find the root's position.
+# S: O(N) No of nodes created for constructing the tree
+
+        
+
+            
+
+
+
+
+
+        
